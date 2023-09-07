48d38552e6cf7168de9edd7c8b4bcb018f0d5c40
おしろ <>
2023-09-07 16:26:45 +0900

    update:GitHub Actionsの設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  experimental: {
+    optimizeFonts: true,
+  },
+  i18n: {
+    locales: ["ja"],
+    defaultLocale: "ja",
+  },
+  router: {
+    base: '/'
+  },
+};
